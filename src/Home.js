9bf6b99d1ff82cf8bfc8ba2347e80ef4516be745
Jesse Levandovski <jessel@triadsystems.com.br>
2021-03-09 12:23:37 -0300

    Upload do projeto novo, clone youtube

---
+++ src/Home.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { 
+    makeStyles, 
+    AppBar, 
+    Toolbar, 
+    Button,
+    IconButton, 
+    Typography
+ } from '@material-ui/core';
+import MenuIcon from "@material-ui/icons/Menu";
+
+const useStyles= makeStyles((theme) => ({
+    root:{      
+        height:'100vh',
+    },
+    appBar:{
+        boxShadow:'none',   
+    },
+    grow:{
+        flexGrow:1,
+    }
+}));
+
+function Home(){
+    
+    const classes = useStyles();
+
+    return <div className={classes.root}>
+        <AppBar position="fixed" color="inherit" className={classes.appBar}>
+        <Toolbar>
+        <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+        <MenuIcon />
+        </IconButton>
+        <div className={classes.grow} />
+        <Button variant="outlined" color="primary">Fazer Login</Button>
+        </Toolbar>
+        </AppBar>
+    </div>;
+}
+
+export default Home;
